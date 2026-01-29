3269aae091290fa967ab5a19e882a5fd91057d4f
RajRajputGit <brajrajput4@gmail.com>
2026-01-29 23:54:23 +0530

    Model Training COmpleted

--- src/logger.py
+++ src/logger.py
@@ -2,18 +2,16 @@ import logging
 import os
 from datetime import datetime
 
-# 1. Create a naming convention for the log file (e.g., 01_22_2026_12_30_00.log)
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
 
-# 2. Create a 'logs' folder in the current directory
-logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
-os.makedirs(os.path.dirname(logs_path), exist_ok=True)
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
-# 3. Basic Configuration
 logging.basicConfig(
-    filename=logs_path,
+    filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
-)
 
-# This allows other files to simply 'from src.logger import logging'
+
+)
